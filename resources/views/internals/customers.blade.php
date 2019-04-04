9011e100f1078578035bcea80c9b6b379056555c
Deepender Rathore <deepenrathore28@gmail.com>
2019-04-04 23:20:00 +1100

    Adding Email For Customers

--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -4,14 +4,23 @@
     <h1>Customers</h1>
     <form action="customers" method="POST">
         {{csrf_field()}}
-        <div class="input-group">
-            <input type="text" name="name" >
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text"  class="form-control {{$errors->first('name') ? 'is-invalid':''}}" id="name" name="name" placeholder="Name" value="{{old('name')}}">
+            <div class="invalid-feedback">{{$errors->first('name')}}</div>
         </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="text" class="form-control {{$errors->first('email') ? 'is-invalid':''}}" id="email" name="email" value="{{old('email')}}" placeholder="Email">
+            <div class="invalid-feedback">{{$errors->first('email')}}</div>
+        </div>
+        <div class="mb-2"></div>
+
         <button type="submit" class="btn btn-primary">Add Customer</button>
     </form>
     <ul>
         @foreach($customers as $customer)
-            <li>{{$customer->name}}</li>
+            <li>{{$customer->name}} ~  <span class="text-muted">{{$customer->email}}</span> </li>
         @endforeach
     </ul>
 @endsection
