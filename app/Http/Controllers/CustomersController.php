98d025cdd55d1d8a49c7612d16857a884320174a
Deepender Rathore <deepenrathore28@gmail.com>
2019-04-04 21:13:51 +1100

    SQLite Database - php artisan migrate and php artisan make:model -m

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -2,17 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Customer;
 use Illuminate\Http\Request;
 
 class CustomersController extends Controller
 {
     public function list(){
-        $customers = [
-            'Deepender',
-            'Narender',
-            'Indu',
-            'Datar'
-        ];
+        $customers = Customer::all();
 
         return view('internals.customers',[
             'customers' => $customers,
