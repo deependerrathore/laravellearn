d4094ee7743e6134db5fc0f523abfd8d27b47a52
Deepender Rathore <deepenrathore28@gmail.com>
2019-04-08 00:15:32 +1000

    Eloquent Scopes & Mass Assignment

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -8,8 +8,8 @@ use Illuminate\Http\Request;
 class CustomersController extends Controller
 {
     public function list(){
-        $activeCustomers = Customer::where('active',1)->get();
-        $inactiveCustomers = Customer::where('active',0)->get();
+        $activeCustomers = Customer::active()->get();
+        $inactiveCustomers = Customer::inactive()->get();
 
         return view('internals.customers',compact('activeCustomers','inactiveCustomers'));
     }
@@ -21,11 +21,7 @@ class CustomersController extends Controller
             'email' => 'required|email',
             'active'=>'required'
         ]);
-        $customer = new Customer();
-        $customer->name = request('name');
-        $customer->email = request('email');
-        $customer->active= request('active');
-        $customer->save();
+        Customer::create($data);
         return back();
 
     }
